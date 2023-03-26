e6d7ca5b70acb753e43f1fecdf061ff63d6a93b0
nguyenhuan2102 <nguyenvanhuan2122001@gmail.com>
2023-03-26 09:45:35 +0700

    init

---
+++ src/test/java/edu/fa/qlktx/QlktxApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fa.qlktx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlktxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
